16c5fad6d21d3566ed350bae25a670f15156d125
Amos Glenn <amos@amosglenn.com>
2025-01-09 23:26:34 -0500

    initial tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import tailwindcssPlugin from 'eslint-plugin-tailwindcss';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('prettier', 'next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
+      tailwindcss: tailwindcssPlugin,
+    },
+    rules: {
+      // Add TypeScript-specific rules here
+      '@typescript-eslint/no-unused-vars': 'warn',
+      // Tailwind CSS rules
+      'tailwindcss/classnames-order': 'warn', // Enforce class name ordering
+      'tailwindcss/enforces-negative-arbitrary-values': 'warn', // Ensure proper arbitrary negative values
+      'tailwindcss/no-contradicting-classname': 'error', // Disallow contradicting class names
+    },
+  },
 ];
 
 export default eslintConfig;
